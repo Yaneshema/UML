adf55e89a31cb99d7ced45683838b331e9ab4aef
SNIR_admin <SNIR_admin@10-SNIR-03.560016n.ac-rennes.fr>
2023-09-14 11:02:40 +0200

    debut class conducteur moto

---
+++ Conducteur.h
@@ -0,0 +1,19 @@
+#endif CONDUCTEUR_H
+#define CONDUCTEUR_H
+#include <iostream>
+#include <string>
+class moto;
+
+class Conducteur
+{
+	private:
+		std::string nom;
+		std::string prenom;
+		int annneeNaissance;
+		Moto *mesMotos[10];
+
+
+
+}
+
+
