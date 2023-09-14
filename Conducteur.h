55a8451a53759d2185d26ab1d6f3e5c9bd5f424f
SNIR_admin <SNIR_admin@10-SNIR-03.560016n.ac-rennes.fr>
2023-09-14 11:59:54 +0200

    enregistrement Conducteur

--- Conducteur.h
+++ Conducteur.h
@@ -6,11 +6,38 @@ class moto;
 
 class Conducteur
 {
-	private:
-		std::string nom;
-		std::string prenom;
-		int annneeNaissance;
-		Moto *mesMotos[10];
+private:
+	std::string nom;
+	std::string prenom;
+	int annneeNaissance;
+	Moto* mesMotos[10];
+
+
+protected
+
+
+public:
+	Conducteur();
+	~Conducteur();
+	Conducteur(const Conducteur& conducteur);
+	Conducteur& operator=(const Conducteur& conducteur);
+
+	Conducteur(std::string nom, std::string prenom, int anneeNaissance, Moto* mesMotos[10]);
+
+	/*void setNom(std::string nom);*/
+	std::string getNom();
+
+	/*void setPrenom(std::string prenom);*/
+	std::string getPrenom();
+
+	int getAnneeNaissance();
+
+	void afficheMotos();
+
+	void addMoto(Moto newMoto);
+
+	void rmMoto(Moto rmMoto);
+
 
 
 
