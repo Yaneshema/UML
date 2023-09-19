1c8717b5ecc6d01cb90f2864650679bc26e71038
Leny <leny.busson69@gmail.com>
2023-09-19 16:44:30 +0200

    jaaj

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,15 @@
 
+#include <iostream>
+#include <string>
+#include "Moteur.h"
+#include "Moto.h"
+#include "Conducteur.h"
+
 int main() {
 
+	Conducteur* Jose = new Conducteur("Jose", "Pash", 1997, NULL);
+	Moto *ZR7 = new Moto(20, NULL);
+	Moteur* ZR7 = new Moteur(20);
+
+	Jose->addMoto(ZR7);
 }
